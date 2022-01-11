697eed1cbb6925c4b4e231077d306c04b5b23ef2
Rupesh Kumar <pra6305@gmail.com>
2022-01-11 12:41:22 +0530

    used referencing

--- models/user.js
+++ models/user.js
@@ -19,12 +19,36 @@ const userSchema = new mongoose.Schema({
         type : String,
         required : true,
     },
-    Stocks:{
-        type: stockInfo.userStocks,
-        required: false
-    }
+    stocksOwned: [{type: Schema.type.ObjectId, ref: 'userStock' }]
 })
+
+var Stock = new Schema([
+    {
+        _id: {
+            type: Schema.Types.ObjectId,  
+            ref: 'User',
+            required: true
+        },
+        itemName: {
+            type: String,
+            required: true
+        },
+        dateAdded: {
+            type: Date,
+            default: new Date()
+        },
+        currentStock:{
+            type: Number,
+            default: 0
+        },
+        manufacturingCompany: {
+            type: String,
+            required: true
+        }
+    }
+]);
+
+var userStocks = mongoose.model('userStock', userStock); 
 userSchema.plugin(passportLocalMongoose);
 const Model = new mongoose.model("User", userSchema);
 module.exports = Model;
-console.log(stockInfo);
